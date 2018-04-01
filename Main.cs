9214ca1fce5d6fda58bc9a80762b16e4b0c77ce6
BuildTools <unconfigured@null.spigotmc.org>
2018-04-01 02:10:13 +0200

    fix() add Main.cs

---
+++ Main.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace chooser
+{
+    class MyProgram
+    {
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.run();
+        }
+        
+    }
+}
